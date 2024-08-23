532f0cdd4c29472ee4c344710a3d8ab2fa49eac1
Nicolás <nicolas@issabel.com>
2024-08-23 16:08:07 -0400

    Do not allow file paths and restrict usage of xmldoc command. Fixes CVE-2024-0986

--- modules/asterisk_cli/index.php
+++ modules/asterisk_cli/index.php
@@ -19,26 +19,30 @@ function _moduleContent(&$smarty, $module_name)
     $local_templates_dir = "$base_dir/modules/$module_name/".$templates_dir.'/'.$arrConf['theme'];
 
     $txtCommand = isset($_POST['txtCommand'])? trim($_POST['txtCommand']) : '';
+
+    $pattern = '/(\/[a-zA-Z0-9_\-\.]+)+/';
+    $cleanedCommand = preg_replace($pattern, '', $txtCommand);
+
     $oForm = new paloForm($smarty, array());
     $smarty->assign(array(
         'asterisk'  =>  _tr('Asterisk CLI'),
         'command'   =>  _tr('Command'),
-        'txtCommand'=>  htmlspecialchars($txtCommand),
+        'txtCommand'=>  htmlspecialchars($cleanedCommand),
         'execute'   =>  _tr('Execute'),
         'icon'      =>  "modules/$module_name/images/pbx_tools_asterisk_cli.png",
     ));
 
     $result = "";
     if (!empty($txtCommand)) {
-        $output = $retval = NULL;
+    	$output = $retval = NULL;
         $ipaddr = $_SERVER['REMOTE_ADDR'];
         $user = isset($_SESSION['issabel_user']) ? $_SESSION['issabel_user'] : 'unknown';
-        if(preg_match("/(originate|system)/i",$txtCommand)) {
-            writeLOG("audit.log", sprintf('ASTCLI %s Command denied: %s from %s.',$user,escapeshellarg($txtCommand),$ipaddr));
+        if(preg_match("/(originate|system|xmldoc)/i",$cleanedCommand)) {
+            writeLOG("audit.log", sprintf('ASTCLI %s Command denied: %s from %s.',$user,escapeshellarg($cleanedCommand),$ipaddr));
             $result = "Command denied";
         } else {
-            exec("/usr/sbin/asterisk -rnx ".escapeshellarg($txtCommand), $output, $retval);
-            writeLOG("audit.log", sprintf('ASTCLI %s: %s from %s.',$user,escapeshellarg($txtCommand),$ipaddr));
+            exec("/usr/sbin/asterisk -rnx ".escapeshellarg($cleanedCommand), $output, $retval);
+            writeLOG("audit.log", sprintf('ASTCLI %s: %s from %s.',$user,escapeshellarg($cleanedCommand),$ipaddr));
             $result = implode("\n", array_map('htmlspecialchars', $output));
         }
     }
